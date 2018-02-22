769231ccfd8caf8872db2d164603edf962c005d5
Kaitlin Kelly <kk504408@gmail.com>
2018-02-22 23:58:00 +0000

    Updated Project Manager class to have secure password, utilize bcrypt gem

--- app/models/project_manager.rb
+++ app/models/project_manager.rb
@@ -1,3 +1,12 @@
 class ProjectManager < ActiveRecord::Base
   has_many :projects
+  has_secure_password
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    ProjectManager.all.find{|pm| pm.slug == slug}
+  end
 end
