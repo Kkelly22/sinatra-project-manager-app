5842503b17ccf2136d85bee51118806ef19b23b9
Kaitlin Kelly <kk504408@gmail.com>
2018-02-27 02:57:25 +0000

    Added routes to create a new client in the controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,6 +3,14 @@ class ClientsController < ApplicationController
   enable :sessions
   use Rack::Flash
 
-  
+  get '/clients/new' do
+    erb :'clients/create_client'
+  end
+
+  post '/clients' do
+      @client = Client.new(:name => params[:name])
+      @client.save
+      erb :'clients/show'
+  end
 
 end
