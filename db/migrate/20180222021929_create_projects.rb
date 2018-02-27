331cf4bcd4d844641590ed3c70217cf62009254a
Kaitlin Kelly <kk504408@gmail.com>
2018-02-27 01:12:37 +0000

    Added new fields I felt were more relavent then my first draft, removed others. Added client ID because I am adding a client class to which a project will also belong to a client.

--- db/migrate/20180222021929_create_projects.rb
+++ db/migrate/20180222021929_create_projects.rb
@@ -1,10 +1,13 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
-      t.string :description
-      t.string :priority
-      t.integer :budget
+      t.integer :project_number
+      t.string :project_name
+      t.integer :delivery_date
+      t.integer :budget_remaining
+      t.string :status
       t.integer :project_manager_id
+      t.integer :client_id
     end
   end
 end
