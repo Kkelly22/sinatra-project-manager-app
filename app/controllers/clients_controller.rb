d416c6aefc7dceb48f2b16d9d79f8b1144bde722
Kaitlin Kelly <kk504408@gmail.com>
2018-02-28 00:57:53 +0000

    Added routes to show/create/edit clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,14 +3,59 @@ class ClientsController < ApplicationController
   enable :sessions
   use Rack::Flash
 
+  get '/clients' do
+    if logged_in?
+      @clients = Client.all
+      erb :'clients/index'
+    else
+      redirect '/login'
+    end
+  end
+
   get '/clients/new' do
-    erb :'clients/create_client'
+    if logged_in?
+      erb :'clients/create_client'
+    else
+      redirect '/login'
+    end
   end
 
   post '/clients' do
-      @client = Client.new(:name => params[:name])
-      @client.save
+    if logged_in?
+      if params[:content] == ""
+        redirect '/clients/new'
+      else
+        @client = Client.new(:name => params[:name])
+        @client.save
+        if @client.save
+          flash[:message] = "Successfully created client."
+          redirect "clients/#{@client.id}"
+        else
+          flash[:message] = "There was an error creating client.  Please try again."
+          redirect '/clients/new'
+        end
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/clients/:id' do
+    if logged_in?
+      @client = Client.find_by_id(params[:id])
       erb :'clients/show'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/clients/:id/edit' do
+    if logged_in?
+      @client = Client.find_by_id(params[:id])
+      erb :'clients/edit'
+    else
+      redirect '/login'
+    end
   end
 
 end
